fe40c06f8434af5e5b966374ee5b9d9fee116daf
Laura Newland <laurasmora101@gmail.com>
2021-07-16 11:33:18 -0500

    added archive.php

---
+++ app/public/wp-content/themes/fictional-university-theme/archive.php
@@ -0,0 +1,57 @@
+<?php 
+
+  get_header(); ?>
+
+  <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>;)"></div>
+      <div class="page-banner__content container container--narrow">
+      
+      <!--this heading statment will handle the title of all archived items. You dont have much control over the output -->
+      <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
+
+      <!-- This gives you more control over the wording for each archive category however you would need to do this for all 
+        <h1 class="page-banner__title"><?php if (is_category()) {
+            single_cat_title();
+            }
+            if (is_author()) {
+                echo 'Post By '; the_author();
+            } ?></h1> 
+        -->
+            
+        <div class="page-banner__intro">
+          <p><?php the_archive_description(); ?></p>
+        </div>
+      </div>
+    </div>
+
+  <div class="container container--narrow page-section">
+    <?php 
+      while (have_posts()) {
+        the_post(); ?>
+
+        <div class="post-item">
+          <h2 class="headline headline--medium headline--post-title">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+          </h2>
+
+          <div class="metabox">
+            <p>Posted By <?php the_author_posts_link(); ?> on <?php the_time('n/d/Y'); ?> in <?php echo get_the_category_list(', ') ?></p>
+          </div>
+
+          <div class="gemeric-content">
+            <?php the_excerpt(); ?>
+            <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue Reading</a></p>
+          </div>
+
+        </div>
+
+        <?php
+      }
+      echo paginate_links();
+    ?>
+  </div>
+
+
+  <?php get_footer();
+
+?>
