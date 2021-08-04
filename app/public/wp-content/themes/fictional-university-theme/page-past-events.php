e4cef078ffa4a5450497670087561583eaab974e
Laura Newland <laurasmora101@gmail.com>
2021-08-04 11:03:34 -0400

    Created and updated page-post-events

--- app/public/wp-content/themes/fictional-university-theme/page-past-events.php
+++ app/public/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -7,10 +7,10 @@
       <div class="page-banner__content container container--narrow">
       
       <!--this heading statment will handle the title of all archived items. You dont have much control over the output -->
-      <h1 class="page-banner__title">All Events</h1>
+      <h1 class="page-banner__title">Past Events</h1>
             
         <div class="page-banner__intro">
-          <p>See hat is going on in the world</p>
+          <p>Recap of our Past Events</p>
         </div>
       </div>
     </div>
@@ -18,8 +18,25 @@
   <div class="container container--narrow page-section">
     
     <?php 
-      while (have_posts()) {
-        the_post(); ?>
+        $today = date('Ymd');
+        $pastEvents = new WP_QUERY(array(
+          'paged' => get_query_var('paged', 1),
+          'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'event_date',
+                'compare' => '<',
+                'value' => $today,
+                'type' => 'numaric'
+              )
+            )
+        ));
+
+      while ($pastEvents->have_posts()) {
+        $pastEvents -> the_post(); ?>
 
             <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
@@ -38,12 +55,10 @@
 
         <?php
       }
-      echo paginate_links();
+      echo paginate_links(array(
+        'total' => $pastEvents->max_num_pages
+      ));
     ?>
-
-      <hr class='section-break'>
-
-      <p>Looking for a recap of past events? <a href='<?php echo site_url('/past-events');?>'>Check out our past events archive!</a></p>
 
   </div>
 
