80d0ee0817350139492d2c4323d885afb46604e0
Laura Newland <laurasmora101@gmail.com>
2021-08-20 11:43:55 -0500

    added professor images

--- app/public/wp-content/themes/fictional-university-theme/single-professor.php
+++ app/public/wp-content/themes/fictional-university-theme/single-professor.php
@@ -17,7 +17,7 @@
         <div class="generica-content">
           <div class="row group">
             <div class="one-third">
-              <?php the_post_thumbnail(); ?>
+              <?php the_post_thumbnail('professorPortrait'); ?>
             </div>
             <div class="two-thirds"></div>
               <?php the_content(); ?>
